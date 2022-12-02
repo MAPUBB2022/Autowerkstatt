716b1337a9b7af974f1573fb2baf559f3a58524d
davidz811 <davidb.coldea@gmail.com>
2022-12-02 17:51:02 +0200

    unit testing on Customer

---
+++ src/test/java/Controller/MechanicControllerTest.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MechanicControllerTest {
+
+
+
+    @Test
+    void getCars() {
+    }
+
+    @Test
+    void getEarnings() {
+    }
+
+    @Test
+    void getRating() {
+    }
+
+    @Test
+    void getCarList() {
+    }
+
+    @Test
+    void updateCar() {
+    }
+
+    @Test
+    void filterByYear() {
+    }
+}
